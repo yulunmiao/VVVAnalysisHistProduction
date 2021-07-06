5d9f9ce2024be5e6ed94649e453c9974469aabe2
yulunmiao <60363605+yulunmiao@users.noreply.github.com>
2021-07-06 15:01:31 -0500

    Update to adapt NanoAODv7 and include cut in class

--- extractMC.sh
+++ extractMC.sh
@@ -32,22 +32,24 @@ mkdir $dir
 #hadd $dir/name.root $jobdir/$year/*/output*
 if [ $year -eq 2018 ]
 then
-	hadd $dir/TTTo2L2Nu.root $jobdir/$year/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/TTToSemiLeptonic.root $jobdir/$year/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext3-v1_NANOAODSIM*/output*
-	hadd $dir/TTWJetsToLNu.root $jobdir/$year/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1_NANOAODSIM*/output*
-	hadd $dir/TTZToLL_M-1to10.root $jobdir/$year/TTZToLL_M-1to10_TuneCP5_13TeV-amcatnlo-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/TTZToLLNuNu_M-10.root $jobdir/$year/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1_NANOAODSIM*/output*
-	hadd $dir/DYJetsToLL_M-10to50.root $jobdir/$year/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1_NANOAODSIM*/output*
-	hadd $dir/DYJetsToLL_M-50.root $jobdir/$year/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WZTo3LNu.root $jobdir/$year/WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1_NANOAODSIM*/output*
-	hadd $dir/WW.root $jobdir/$year/WW_TuneCP5_13TeV-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WW_DoubleScattering.root $jobdir/$year/WW_DoubleScattering_13TeV-pythia8_TuneCP5_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1_NANOAODSIM*/output*
-	hadd $dir/WpWpJJ.root $jobdir/$year/WpWpJJ_EWK-QCD_TuneCP5_13TeV-madgraph-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/ZZTo4L.root $jobdir/$year/ZZTo4L_TuneCP5_13TeV_powheg_pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext2-v1_NANOAODSIM*/output*
-	hadd $dir/WJetsToLNu_HT-100To200.root $jobdir/$year/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WJetsToLNu_HT-200To400.root $jobdir/$year/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WJetsToLNu_HT-400To600.root $jobdir/$year/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WJetsToLNu_HT-600To800.root $jobdir/$year/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WJetsToLNu_HT-800To1200.root $jobdir/$year/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM*/output*
-	hadd $dir/WWW.root $jobdir/$year/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1_NANOAODSIM*/output*
+	hadd $dir/TTTo2L2Nu.root $jobdir/$year/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8*/output*
+	hadd $dir/TTToSemiLeptonic.root $jobdir/$year/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8*/output*
+	hadd $dir/TTWJetsToLNu.root $jobdir/$year/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8*/output*
+	hadd $dir/TTZToLL_M-1to10.root $jobdir/$year/TTZToLL_M-1to10_TuneCP5_13TeV-amcatnlo-pythia8*/output*
+	hadd $dir/TTZToLLNuNu_M-10.root $jobdir/$year/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8*/output*
+	hadd $dir/DYJetsToLL_M-10to50.root $jobdir/$year/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/DYJetsToLL_M-50.root $jobdir/$year/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WZTo3LNu.root $jobdir/$year/WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8*/output*
+	hadd $dir/WW.root $jobdir/$year/WW_TuneCP5_13TeV-pythia8*/output*
+	hadd $dir/WW_DoubleScattering.root $jobdir/$year/WW_DoubleScattering_13TeV-pythia8*/output*
+	hadd $dir/WpWpJJ.root $jobdir/$year/WpWpJJ_EWK-QCD_TuneCP5_13TeV-madgraph-pythia8*/output*
+	hadd $dir/ZZTo4L.root $jobdir/$year/ZZTo4L_TuneCP5_13TeV_powheg_pythia8*/output*
+	hadd $dir/WJetsToLNu_HT-100To200.root $jobdir/$year/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WJetsToLNu_HT-200To400.root $jobdir/$year/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WJetsToLNu_HT-400To600.root $jobdir/$year/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WJetsToLNu_HT-600To800.root $jobdir/$year/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WJetsToLNu_HT-800To1200.root $jobdir/$year/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WJetsToLNu_HT-1200To2500.root $jobdir/$year/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+        hadd $dir/WJetsToLNu_HT-2500ToInf.root $jobdir/$year/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8*/output*
+	hadd $dir/WWW.root $jobdir/$year/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8*/output*
 fi
--- make_hist.h
+++ make_hist.h
@@ -70,20 +70,16 @@ struct Event{
         float mll;
         float ht;
         float st;
+	float deltaRll;
         float deltaphill;
 	float njet_overlapremoved;
 	float nbloose;
 	float nbmedium;
-	float nbloose_overlapremoved;
-	float nbmedium_overlapremoved;
 };
 
-bool addtion_cut(const Event& evt){
-	return true;
-}
 
 class HistCollection{
-        private:
+        protected:
         TH1F* hlep1pt;
         TH1F* hlep2pt;
         TH1F* hlep1eta;
@@ -107,16 +103,15 @@ class HistCollection{
         TH1F* hnjet;
 	TH1F* hnjet_overlapremoved;
         TH1F* hnbloose;
-        TH1F* hnbloose_overlapremoved;
-	TH1F* hnbloose_overlap;
 	TH1F* hnbmedium;
-	TH1F* hnbmedium_overlapremoved;
-	TH1F* hnbmedium_overlap;
         TH1F* hnfatjet;
         TH1F* hmll;
         TH1F* hst;
 	TH1F* hht;
+	TH1F* hdeltaRll;
         TH1F* hdeltaphill;
+	TH2F* hpt1pt2;
+	virtual bool cut(Event &evt);
         public:
         HistCollection();
         ~HistCollection();
@@ -139,7 +134,7 @@ HistCollection::HistCollection(){
         hfatjettau2=	new TH1F("hfatjettau2","fatjet #tau_{2};#tau_{2};Events",1000,0,1);
         hfatjettau21=	new TH1F("hfatjettau21","fatjet #tau_{21};#tau_{21};Events",1000,0,1);
         hfatjetdeepMDW=	new TH1F("hfatjetdeepMDW","fatjet deepMD W;;Events",1000,0,1);
-        hfatjetdeepW=	new TH1F("hfatjetdeepW","fatjet deep W;;Evnets",1000,0,1);
+        hfatjetdeepW=	new TH1F("hfatjetdeepW","fatjet deep W;;Evnets",200,0,1);
         hfatjetdeepMDZ= new TH1F("hfatjetdeepMDZ","fatjet deepMD Z;;Events",1000,0,1);
         hfatjetdeepZ=	new TH1F("hfatjetdeepZ","fatjet deep Z;;Events",1000,0,1);
         hfatjetdeepT=	new TH1F("hfatjetdeepT","fatjet deep T;;Events",1000,0,1);
@@ -148,16 +143,14 @@ HistCollection::HistCollection(){
         hnjet=		new TH1F("hnjet","number of jets;n_{jet};Events",10,0,10);
 	hnjet_overlapremoved=new TH1F("hnjet_overlapremoved","number of jets;n_{jet};Events",10,0,10);
         hnbloose=	new TH1F("hnbloose","number of loose b jets;n_{bjet};Events",10,0,10);
-        hnbloose_overlapremoved=new TH1F("hnbloose_overlapremoved","number of loose b jets;n_{bjet};Events",10,0,10);
-	hnbloose_overlap=new TH1F("hnbloose_overlap","nubmer of loose b jets;n_{bjet};Events",10,0,10);
 	hnbmedium=	new TH1F("hnbmedium","number of medium b jets;n_{bjet};Events",10,0,10);
-        hnbmedium_overlapremoved=new TH1F("hbmedium_overlapremoved","number of medium b jets;n_{bjet};Events",10,0,10);
-	hnbmedium_overlap=new TH1F("hnbmedium_overlap","nubmer of medium b jets;n_{bjet};Events",10,0,10);
         hnfatjet=	new TH1F("hnfatjet","number of fatjets;n_{fatjet};Events",5,0,5);
         hmll=		new TH1F("hmll","invariant mass of lepton pair;m_{ll};Events",1000,0,1000);
         hst=		new TH1F("hst","S_{T};S_{T}(GeV);Events",2000,0,2000);
         hht=		new TH1F("hht","H_{T};H_{T}(GeV);Events",2000,0,2000);
-        hdeltaphill=	new TH1F("hdeltaphill","#delta#phi between lepton pair;#delta#phi_{ll};Events",800,0,4);
+	hdeltaRll=	new TH1F("hdeltaRll","#DeltaR between lepton pair;#DeltaR_{ll};Events",800,0,4);
+        hdeltaphill=	new TH1F("hdeltaphill","#Delta#phi between lepton pair;#Delta#phi_{ll};Events",800,0,4);
+	hpt1pt2=	new TH2F("hpt1pt2","2D histogram of p_{T,1} and p_{T,2};p_{T,1};p_{T,2};Events",36,10,100,36,10,100);
 }
 
 HistCollection::~HistCollection(){
@@ -184,18 +177,21 @@ HistCollection::~HistCollection(){
         delete hnjet;
 	delete hnjet_overlapremoved;
         delete hnbloose;
-        delete hnbloose_overlapremoved;
-	delete hnbloose_overlap;
 	delete hnbmedium;
-        delete hnbmedium_overlapremoved;
-	delete hnbmedium_overlap;
         delete hnfatjet;
         delete hmll;
         delete hst;
         delete hht;
         delete hdeltaphill;
+	delete hdeltaRll;
+	delete hpt1pt2;
 
 }
+
+bool HistCollection::cut(Event &evt){
+	return true;
+}
+
 
 bool HistCollection::Fill(const float lumi,const float cross_section,const char* path){
         TChain *chain=new TChain("t");
@@ -204,21 +200,24 @@ bool HistCollection::Fill(const float lumi,const float cross_section,const char*
 	LV *met=0;
 	vector<bool> *jetbloose=0,*jetbmedium=0;
 	vector<float> *fatjetsdm=0,*tau21=0,*tau2=0,*tau1=0,*deepMDW=0,*deepW=0,*deepMDZ=0,*deepZ=0,*deepT=0,*deepMDbb=0;
-        chain->SetBranchAddress("SS2jet_lep_p4",                &lepp4);
-        chain->SetBranchAddress("SS2jet_jet_p4",                &jetp4);
-        chain->SetBranchAddress("SS2jet_jet_passBloose",        &jetbloose);
-        chain->SetBranchAddress("SS2jet_jet_passBmedium",       &jetbmedium);
-        chain->SetBranchAddress("SS2jet_fatjet_p4",             &fatjetp4);
-        chain->SetBranchAddress("SS2jet_fatjet_msoftdrop",      &fatjetsdm);
-        chain->SetBranchAddress("SS2jet_fatjet_tau21",          &tau21);
-        chain->SetBranchAddress("SS2jet_fatjet_tau2",           &tau2);
-        chain->SetBranchAddress("SS2jet_fatjet_tau1",           &tau1);
-        chain->SetBranchAddress("SS2jet_fatjet_deepMD_W",       &deepMDW);
-        chain->SetBranchAddress("SS2jet_fatjet_deep_W",         &deepW);
-        chain->SetBranchAddress("SS2jet_fatjet_deepMD_Z",       &deepMDZ);
-        chain->SetBranchAddress("SS2jet_fatjet_deep_Z",         &deepZ);
-        chain->SetBranchAddress("SS2jet_fatjet_deep_T",         &deepT);
-        chain->SetBranchAddress("SS2jet_fatjet_deepMD_bb",      &deepMDbb);
+	int nbmedium,nbloose;
+        chain->SetBranchAddress("Common_lep_p4",                &lepp4);
+        chain->SetBranchAddress("Common_jet_p4",                &jetp4);
+	chain->SetBranchAddress("Common_jet_passBloose",	&jetbloose);
+	chain->SetBranchAddress("Common_jet_passBmedium",       &jetbmedium);
+	chain->SetBranchAddress("Common_nb_loose",		&nbloose);
+	chain->SetBranchAddress("Common_nb_medium",             &nbmedium);
+        chain->SetBranchAddress("Common_fatjet_p4",             &fatjetp4);
+        chain->SetBranchAddress("Common_fatjet_msoftdrop",      &fatjetsdm);
+        chain->SetBranchAddress("Common_fatjet_tau21",          &tau21);
+        chain->SetBranchAddress("Common_fatjet_tau2",           &tau2);
+        chain->SetBranchAddress("Common_fatjet_tau1",           &tau1);
+        chain->SetBranchAddress("Common_fatjet_deepMD_W",       &deepMDW);
+        chain->SetBranchAddress("Common_fatjet_deep_W",         &deepW);
+        chain->SetBranchAddress("Common_fatjet_deepMD_Z",       &deepMDZ);
+        chain->SetBranchAddress("Common_fatjet_deep_Z",         &deepZ);
+        chain->SetBranchAddress("Common_fatjet_deep_T",         &deepT);
+        chain->SetBranchAddress("Common_fatjet_deepMD_bb",      &deepMDbb);
         chain->SetBranchAddress("Common_met_p4",		&met);
 	chain->Add(path);
         TFile *file=TFile::Open(path);
@@ -234,8 +233,6 @@ bool HistCollection::Fill(const float lumi,const float cross_section,const char*
 		evt.st=0;
 		evt.nbloose=0;
 		evt.nbmedium=0;
-		evt.nbloose_overlapremoved=0;
-		evt.nbmedium_overlapremoved=0;
 		evt.njet_overlapremoved=0;
 
 		for(unsigned int ilep=0;ilep<lepp4->size();ilep++){
@@ -292,21 +289,16 @@ bool HistCollection::Fill(const float lumi,const float cross_section,const char*
 				evt.ht+=jetp4->at(ijet).pt();
 				evt.njet_overlapremoved++;
 			}
-			if(jetbloose->at(ijet)){
-				evt.nbloose++;
-				if(!overlap) evt.nbloose_overlapremoved++;
-			}
-			if(jetbmedium->at(ijet)){
-				evt.nbmedium++;
-				if(!overlap) evt.nbmedium_overlapremoved++;
-			}
 		}
+		evt.nbmedium=nbmedium;
+		evt.nbloose=nbloose;
 		evt.st+=met->pt();
 		evt.mll=(lepp4->at(0)+lepp4->at(1)).M();
+		evt.deltaRll=fabs(ROOT::Math::VectorUtil::DeltaR(lepp4->at(0),lepp4->at(1)));
 		evt.deltaphill=fabs(ROOT::Math::VectorUtil::DeltaPhi(lepp4->at(0),lepp4->at(1)));
 		//build the event
 		//fill in histograms
-		if(addtion_cut(evt)){//addition cut
+		if(cut(evt)){//addition cut
 			hlep1pt->Fill(evt.lep.at(0).pt,			evt.scale);
 			hlep2pt->Fill(evt.lep.at(1).pt,			evt.scale);
 			hlep1eta->Fill(evt.lep.at(0).eta,		evt.scale);
@@ -330,18 +322,16 @@ bool HistCollection::Fill(const float lumi,const float cross_section,const char*
 			hnjet->Fill(evt.jet.size(),			evt.scale);
 			hnjet_overlapremoved->Fill(evt.njet_overlapremoved,evt.scale);
 			hnbloose->Fill(evt.nbloose,			evt.scale);
-			hnbloose_overlapremoved->Fill(evt.nbloose_overlapremoved,evt.scale);
-			hnbloose_overlap->Fill(evt.nbloose-evt.nbloose_overlapremoved,evt.scale);
 			hnbmedium->Fill(evt.nbmedium,			evt.scale);
-			hnbmedium_overlapremoved->Fill(evt.nbmedium_overlapremoved,evt.scale);
-			hnbmedium_overlap->Fill(evt.nbmedium-evt.nbmedium_overlapremoved,evt.scale);
 			hnfatjet->Fill(evt.fatjet.size(),		evt.scale);
 			hmll->Fill(evt.mll,				evt.scale);
 			hst->Fill(evt.st,				evt.scale);
 			hht->Fill(evt.ht,				evt.scale);
+			hdeltaRll->Fill(evt.deltaRll,			evt.scale);
 			hdeltaphill->Fill(evt.deltaphill,		evt.scale);
-
+			hpt1pt2->Fill((evt.lep.at(0).pt<100?evt.lep.at(0).pt:98),(evt.lep.at(1).pt<100?evt.lep.at(1).pt:98),evt.scale);
 		}
+		
 		//fill in histograms
         }
         return true;
@@ -372,16 +362,15 @@ bool HistCollection::Write(const char* path){
         hnjet->Write();
 	hnjet_overlapremoved->Write();
         hnbloose->Write();
-        hnbloose_overlapremoved->Write();
-	hnbloose_overlap->Write();
 	hnbmedium->Write();
-        hnbmedium_overlapremoved->Write();
-	hnbmedium_overlap->Write();
         hnfatjet->Write();
         hmll->Write();
         hst->Write();
         hht->Write();
+	hdeltaRll->Write();
         hdeltaphill->Write();
+	hpt1pt2->Write();
+	file->Close();
 	return true;
 }
 
